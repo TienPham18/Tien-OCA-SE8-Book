41663607b941411d4a339c7a03b57eb5a4888da0
Tien Pham <tienphamvanfuv@gmail.com>
2024-03-30 15:35:01 +0100

    Konstruktor Basic

---
+++ src/Objekt_orientierung/Konstrukturen/Konstruktor_Basic.java
@@ -0,0 +1,19 @@
+package Objekt_orientierung.Konstrukturen;
+
+public class Konstruktor_Basic {
+}
+
+class Foo1 {
+    Foo1() { } // die konstruktor für die klass Foo
+
+}
+
+class Foo2 {
+    int size;
+    String name;
+    Foo2(String name, int size){
+        this.name = name;
+        this.size = size;
+    }
+}
+
