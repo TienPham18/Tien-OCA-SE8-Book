2517c1475998f7450d0e07915236be189e6e4d2d
Tien Pham <tienphamvanfuv@gmail.com>
2024-05-27 17:42:30 +0200

    Short-Curcuit Operator TestOR

--- src/T04_Operators/LogicalOperators/ShortCurcuit/Test.java
+++ src/T04_Operators/LogicalOperators/ShortCurcuit/Test.java
@@ -1,4 +1,22 @@
 package T04_Operators.LogicalOperators.ShortCurcuit;
 
-public class Test {
+class TestOR {
+    public static void main(String[] args) {
+        if ((isItSmall(3)) || (isItSmall(7))) {
+            System.out.println("Result of first if-statement is true");
+        }
+        if ((isItSmall(7)) || (isItSmall(3))) {
+            System.out.println("Result of second if-statement is true");
+        }
+    }
+
+    public static boolean isItSmall(int i) {
+        if (i < 5) {
+            System.out.println("i < 5");
+            return true;
+        } else {
+            System.out.println("i >= 5");
+            return false;
+        }
+    }
 }
