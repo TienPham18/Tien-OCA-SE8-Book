4149fca11015a27227e25c04ce92f74db4d04572
Tien Pham <tienphamvanfuv@gmail.com>
2024-05-27 21:33:54 +0200

    switch expression type char and case label type byte

--- src/T05_FlowControl_Exceptions/p01_ControlFlow/DecisionMakingStatement/Switch/TypeChar.java
+++ src/T05_FlowControl_Exceptions/p01_ControlFlow/DecisionMakingStatement/Switch/TypeChar.java
@@ -4,9 +4,17 @@ public class TypeChar {
     public static void main(String[] args) {
         int x = 1; // compile but print nothing in console
         //Integer x = 1; // Compiler-error
-        switch (x) {
+        switch (x) { // switch expression type int and case label value of type char
             case 'a':
                 System.out.println("a");
+        }
+
+        // a byte might have negative values
+        char ch = 'x';
+        switch(ch){
+            // case -1 : System.out.println("-1"); break; // case -1 compiler-error
+
+            default: System.out.println("default");
         }
     }
 }
